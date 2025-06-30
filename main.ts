75e1e079d7159e23d6c28f11cd79357924f5cc7d
GlowyGhost <jakubklimczak2013@gmail.com>
2025-06-30 17:05:22 +0100

    Added a selection before selecting file
    Needs a bit of fixing

--- main.ts
+++ main.ts
@@ -1,16 +1,55 @@
 import Parser from "./frontend/parser.ts";
-import { createGlobalEnv } from "./runtime/environment.ts";
+import Environment, { createGlobalEnv } from "./runtime/environment.ts";
 import { evaluate } from "./runtime/interpreter.ts";
 
-run("./test.txt");
+findMode();
 
-async function run(filename: string) {
+async function run(filename: string, env: Environment) {
 	const parser = new Parser();
-	const env = createGlobalEnv();
 
 	const input = await Deno.readTextFile(filename);
 	const program = parser.produceAST(input);
 
 	const result = evaluate(program, env);
-	// console.log(result);
+	//console.log(result);
+
+	Deno.exit(1);
+}
+
+async function findMode() {
+	const env = createGlobalEnv();
+
+	while (true) {
+		console.log("repl | exit | file")
+		const input = prompt("Mode > ");
+
+		if (input == "exit") {
+			Deno.exit(0);
+		} else if (input == "file") {
+			const input2 = prompt("fileDir > ");
+			if (!input2 && input2 != undefined) {
+				await run(input2, env);
+				Deno.exit(0);
+			}
+		} else if (input == "repl") {
+			await repl(env);
+			Deno.exit(0);
+		}
+	}
+}
+
+async function repl(env: Environment) {
+  	const parser = new Parser();
+  	console.log("\nRepl v0.1");
+
+  	while (true) {
+    	const input = prompt("> ");
+    	if (!input || input.includes("exit")) {
+      		break;
+    	}
+
+    	const program = parser.produceAST(input);
+    	
+		const result = evaluate(program, env)
+  	}
 }
