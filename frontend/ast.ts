e15978e2bb96e6a92547d5e46f1a44a02454d299
GlowyGhost <jakubklimczak2013@gmail.com>
2025-06-30 17:08:30 +0100

    Compatibility with strings

--- frontend/ast.ts
+++ frontend/ast.ts
@@ -1,37 +1,26 @@
-// https://github.com/tlaceby/guide-to-interpreters-series
-
-// -----------------------------------------------------------
-// --------------          AST TYPES        ------------------
-// ---     Defines the structure of our languages AST      ---
-// -----------------------------------------------------------
-
 export type NodeType =
 	// STATEMENTS
 	| "Program"
 	| "VarDeclaration"
 	| "FunctionDeclaration"
+
 	// EXPRESSIONS
 	| "AssignmentExpr"
 	| "MemberExpr"
 	| "CallExpr"
+
 	// Literals
 	| "Property"
 	| "ObjectLiteral"
 	| "NumericLiteral"
+	| "StringLiteral"
 	| "Identifier"
 	| "BinaryExpr";
 
-/**
- * Statements do not result in a value at runtime.
- They contain one or more expressions internally */
 export interface Stmt {
 	kind: NodeType;
 }
 
-/**
- * Defines a block which contains many statements.
- * -  Only one program will be contained in a file.
- */
 export interface Program extends Stmt {
 	kind: "Program";
 	body: Stmt[];
@@ -51,7 +40,6 @@ export interface FunctionDeclaration extends Stmt {
 	body: Stmt[];
 }
 
-/**  Expressions will result in a value at runtime unlike Statements */
 export interface Expr extends Stmt {}
 
 export interface AssignmentExpr extends Expr {
@@ -60,16 +48,11 @@ export interface AssignmentExpr extends Expr {
 	value: Expr;
 }
 
-/**
- * A operation with two sides seperated by a operator.
- * Both sides can be ANY Complex Expression.
- * - Supported Operators -> + | - | / | * | %
- */
 export interface BinaryExpr extends Expr {
 	kind: "BinaryExpr";
 	left: Expr;
 	right: Expr;
-	operator: string; // needs to be of type BinaryOperator
+	operator: string;
 }
 
 export interface CallExpr extends Expr {
@@ -85,21 +68,19 @@ export interface MemberExpr extends Expr {
 	computed: boolean;
 }
 
-// LITERAL / PRIMARY EXPRESSION TYPES
-/**
- * Represents a user-defined variable or symbol in source.
- */
 export interface Identifier extends Expr {
 	kind: "Identifier";
 	symbol: string;
 }
 
-/**
- * Represents a numeric constant inside the soure code.
- */
 export interface NumericLiteral extends Expr {
 	kind: "NumericLiteral";
 	value: number;
+}
+
+export interface StringLiteral extends Expr {
+	kind: "StringLiteral";
+	value: string;
 }
 
 export interface Property extends Expr {
